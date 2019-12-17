9f8635e2b9b53bd6c0a11952b3762e9ce303fbb0
Minh Nhut Nguyen <Myn@MacBook-Air-de-Minh.local>
2019-12-17 14:02:15 +0100

    fix: changed test name

--- tests/city_test.py
+++ tests/city_test.py
@@ -2,8 +2,8 @@ import apps.cities.city as city
 from settings import CSV_CITY
 
 
-def test_golden_master():
-    citytest = str(city.extract_and_prepare_cities_from_csv(csv_city))
+def test_main_data():
+    citytest = str(city.extract_and_prepare_cities_from_csv(CSV_CITY))
     with open('tests/results/golden_master.txt', 'r') as file:
         data = file.read()
     assert citytest == data
